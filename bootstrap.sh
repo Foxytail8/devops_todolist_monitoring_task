32798059aeaef636b74c7a606dd5f8f49227b364
Foxytail8 <xinenax@gmail.com>
2026-06-25 16:12:05 +0300

    'Solution3'

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,10 @@ for node in $(kubectl get nodes -l app=mysql -o name); do
   kubectl taint "$node" app=mysql:NoSchedule
 done
 
+# Install prometheus from web
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack --version 87.2.0 --namespace monitoring --create-namespace
+
+# Install  todoapp
 helm install todoapp .infrastructure/helm-chart/todoapp
